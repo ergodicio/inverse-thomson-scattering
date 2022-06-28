caaf7005e1b7bee94d6596a9056d0a646c7c741c
archis <joglekara@users.noreply.github.com>
2022-06-27 20:26:48 -0700

    working jax version

---
+++ inverse_thomson_scattering/jax/form_factor.py
@@ -0,0 +1,208 @@
+from jax import numpy as jnp
+from jax import jit
+from inverse_thomson_scattering.jax import ratintn
+from inverse_thomson_scattering.v0 import lamParse
+from inverse_thomson_scattering.v0.form_factor import zprimeMaxw
+
+
+def get_form_factor_fn(lamrang, lam):
+    npts = 20460
+
+    # basic quantities
+    C = 2.99792458e10
+    Me = 510.9896 / C**2  # electron mass keV/C^2
+    Mp = Me * 1836.1  # proton mass keV/C^2
+    [omgL, omgs, lamAxis, _] = lamParse.lamParse(lamrang, lam, npts, True)
+
+    h = 0.01
+    minmax = 8.2
+    h1 = 1000
+    xi1 = jnp.linspace(-minmax - jnp.sqrt(2.0) / h1, minmax + jnp.sqrt(2.0) / h1, h1)
+    xi2 = jnp.array(jnp.arange(-minmax, minmax, h))
+    Zpi = jnp.array(zprimeMaxw(xi2))
+
+    # def vmap_interp(xie, chiERratprim):
+    #     return jnp.interp(xie, xi2, chiERratprim)
+
+    # @jit
+    def nonMaxwThomson(Te, Ti, Z, A, fract, ne, Va, ud, sa, fe):
+        """
+        NONMAXWTHOMSON calculates the Thomson spectral density function S(k,omg) and is capable of handeling multiple plasma
+         conditions and scattering angles. The spectral density function is calculated with and without the ion contribution
+         which can be set to an independent grid from the electron contribution. Distribution functions can be one or two
+         dimensional and the appropriate susceptibility is calculated with the rational integration.
+        :param Te: electron temperature in keV [1 by number of plasma conditions]
+        :param Ti: ion temperature in keV [1 by number of ion species]
+        :param Z: ionization state [1 by number of ion species]
+        :param A: atomic mass [1 by number of ion species]
+        :param fract: relative ion composition [1 by number of ion species]
+        :param ne: electron density in 1e20 cm^-3 [1 by number of plasma conditions]
+        :param Va: flow velocity
+        :param ud: drift velocity
+        :param lamrang: wavelength range in nm [1 by 2]
+        :param lam: probe wavelength in nm
+        :param sa: scattering angle in degrees [1 by n]
+        :param fe: Distribution function (DF) and normalized velocity (x) for 1D distributions and
+        Distribution function (DF), normalized velocity (x), and angles from k_L to f1 in radians
+        :return:
+        """
+
+        Mi = jnp.array(A) * Mp  # ion mass
+        re = 2.8179e-13  # classical electron radius cm
+        Esq = Me * C**2 * re  # sq of the electron charge keV cm
+        constants = jnp.sqrt(4 * jnp.pi * Esq / Me)
+        sarad = sa * jnp.pi / 180  # scattering angle in radians
+        sarad = jnp.reshape(sarad, [1, 1, -1])
+
+        Va = Va * 1e6  # flow velocity in 1e6 cm/s
+        ud = ud * 1e6  # drift velocity in 1e6 cm/s
+
+        # calculate k and omega vectors
+        omgpe = constants * jnp.sqrt(jnp.transpose(ne))  # plasma frequency Rad/cm
+        omg = omgs - omgL
+        omg = omg[jnp.newaxis, ..., jnp.newaxis]
+
+        ks = jnp.sqrt(omgs**2 - omgpe**2) / C
+        ks = ks[jnp.newaxis, ..., jnp.newaxis]
+        kL = jnp.sqrt(omgL**2 - omgpe**2) / C
+        k = jnp.sqrt(ks**2 + kL**2 - 2 * ks * kL * jnp.cos(sarad))
+
+        kdotv = k * Va
+        omgdop = omg - kdotv
+
+        # plasma parameters
+
+        # electrons
+        vTe = jnp.sqrt(Te / Me)  # electron thermal velocity
+        klde = (vTe / omgpe) * k
+
+        # ions
+        Z = jnp.reshape(Z, [1, 1, 1, -1])
+        # A = jnp.reshape(A, [1, 1, 1, -1])
+        Mi = jnp.reshape(Mi, [1, 1, 1, -1])
+        fract = jnp.reshape(fract, [1, 1, 1, -1])
+        Zbar = jnp.sum(Z * fract)
+        ni = fract * ne / Zbar
+        omgpi = constants * Z * jnp.sqrt(ni * Me / Mi)
+
+        vTi = jnp.sqrt(Ti / Mi)  # ion thermal velocity
+        # kldi = jnp.transpose(vTi / omgpi, [1, 0, 2, 3]) * k
+        kldi = (vTi / omgpi) * (k[..., jnp.newaxis])
+
+        # ion susceptibilities
+        # finding derivative of plasma dispersion function along xii array
+        # proper handeling of multiple ion temperatures is not implemented
+        xii = 1.0 / jnp.transpose((jnp.sqrt(2.0) * vTi), [1, 0, 2, 3]) * ((omgdop / k)[..., jnp.newaxis])
+        num_species = len(fract)
+        num_ion_pts = jnp.shape(xii)
+        chiI = jnp.zeros(num_ion_pts)
+
+        # ZpiR = sp.interp1d(xi2, Zpi[0, :], "cubic", bounds_error=False, fill_value=0)
+        ZpiR = jnp.interp(xii, xi2, Zpi[0, :])  # , "cubic", bounds_error=False, fill_value=0)
+        # ZpiI = sp.interp1d(xi2, Zpi[1, :], "cubic", bounds_error=False, fill_value=0)
+        ZpiI = jnp.interp(xii, xi2, Zpi[1, :])  # , "cubic", bounds_error=False, fill_value=0)
+        chiI = jnp.sum(-0.5 / (kldi**2) * (ZpiR + jnp.sqrt(-1 + 0j) * ZpiI), 3)
+
+        # electron susceptibility
+        # calculating normilized phase velcoity(xi's) for electrons
+        xie = omgdop / (k * vTe) - ud / vTe
+
+        # capable of handling isotropic or anisotropic distribution functions
+        # fe is separated into components distribution function, v / vth axis, angles between f1 and kL
+        # if len(fe) == 2:
+        [DF, x] = fe
+        fe_vphi = jnp.exp(jnp.interp(xie, x, jnp.log(DF)))  # , interpAlg, bounds_error=False, fill_value=-jnp.inf)
+        # fe_vphi = jnp.exp(fe_vphi(xie))
+        #    jnp.exp(sp.interp1d(x, jnp.log(DF), xie, interpAlg, -jnp.inf))
+        # fe_vphi[jnp.isnan(fe_vphi)] = 0.0
+
+        # elif len(fe) == 3:
+        #     [DF, x, thetaphi] = fe
+        #     # the angle each k makes with the anisotropy is calculated
+        #     thetak = jnp.pi - jnp.arcsin((ks / k) * jnp.sin(sarad))
+        #     thetak[:, omg < 0, :] = -jnp.arcsin((ks[omg < 0] / k[:, omg < 0, :]) * jnp.sin(sarad))
+        #     # arcsin can only return values from -pi / 2 to pi / 2 this attempts to find the real value
+        #     theta90 = jnp.arcsin((ks / k) * jnp.sin(sarad))
+        #     ambcase = bool((ks > kL / jnp.cos(sarad)) * (sarad < jnp.pi / 2))
+        #     thetak[ambcase] = theta90[ambcase]
+        #
+        #     beta = jnp.arccos(
+        #         jnp.sin(thetaphi[1]) * jnp.sin(thetaphi[0]) * jnp.sin(thetak) + jnp.cos(thetaphi[0]) * jnp.cos(thetak)
+        #     )
+        #
+        #     # here the abs(xie) handles the double counting of the direction of k changing and delta omega being negative
+        #     fe_vphi = jnp.exp(
+        #         jnp.interpn(
+        #             jnp.arange(0, 2 * jnp.pi, 10**-1.2018), x, jnp.log(DF), beta, jnp.abs(xie), interpAlg, -jnp.inf
+        #         )
+        #     )
+        #
+        #     fe_vphi[jnp.isnan(fe_vphi)] = 0
+
+        df = jnp.diff(fe_vphi, 1, 1) / jnp.diff(xie, 1, 1)
+        df = jnp.append(df, jnp.zeros((1, 1, len(sa))), 1)
+
+        chiEI = jnp.pi / (klde**2) * jnp.sqrt(-1 + 0j) * df
+
+        # ratmod = sp.interp1d(x, jnp.log(DF), interpAlg, bounds_error=False, fill_value=-jnp.inf)
+        ratmod = jnp.exp(jnp.interp(xi1, x, jnp.log(DF)))  # , interpAlg, bounds_error=False, fill_value=-jnp.inf)
+        # ratmod = jnp.array(jnp.exp(ratmod(xi1)), dtype=float)
+        ratdf = jnp.gradient(ratmod, xi1[1] - xi1[0])
+        # ratdf[jnp.isnan(ratdf)] = 0
+
+        chiERratprim = jnp.array([jnp.real(ratintn.ratintn(ratdf, xi1 - xi2[iw], xi1)) for iw in range(len(xi2))])
+
+        # if len(fe) == 2:
+        # not sure about the extrapolation here
+        # chiERrat = sp.interp1d(xi2, chiERratprim, "cubic", bounds_error=False, fill_value=0)
+        # chiERrat = jnp.squeeze(chiERrat(xie), 0)
+        chiERrat = jnp.transpose(
+            jnp.concatenate([jnp.interp(xie[0, :, i], xi2, chiERratprim[:, 0])[None, :] for i in range(xie.shape[2])])
+        )
+        # else:
+        #     chiERrat = jnp.interpn(jnp.arange(0, 2 * jnp.pi, 10**-1.2018), xi2, chiERratprim, beta, xie, "spline")
+        chiERrat = -1.0 / (klde**2) * chiERrat
+        # plt.plot(chiERrat[0, :, 1])
+
+        chiE = chiERrat + chiEI
+        # print(chiEI)
+        # plt.plot(jnp.imag(chiEI[0, :, 1]))
+        epsilon = 1.0 + chiE + chiI
+        # plt.plot(jnp.real(epsilon[0,:,1]))
+
+        # This line needs to be changed if ion distribution is changed!!!
+        # ion_comp = Z. * sqrt(Te / Ti. * A * 1836) * (abs(chiE)). ^ 2. * exp(-(xii. ^ 2)) / sqrt(2 * pi);
+        ion_comp_fact = jnp.transpose(fract * Z**2 / Zbar / vTi, [1, 0, 2, 3])
+        ion_comp = ion_comp_fact * (
+            (jnp.abs(chiE[..., jnp.newaxis])) ** 2.0 * jnp.exp(-(xii**2)) / jnp.sqrt(2 * jnp.pi)
+        )
+        ele_comp = (jnp.abs(1.0 + chiI)) ** 2.0 * fe_vphi / vTe
+        ele_compE = fe_vphi / vTe
+
+        SKW_ion_omg = (
+            2.0
+            * jnp.pi
+            * 1.0
+            / klde[..., jnp.newaxis]
+            * (ion_comp)
+            / ((jnp.abs(epsilon[..., jnp.newaxis])) ** 2)
+            * 1.0
+            / omgpe
+        )
+        SKW_ion_omg = jnp.sum(SKW_ion_omg, 3)
+        SKW_ele_omg = 2 * jnp.pi * 1.0 / klde * (ele_comp) / ((jnp.abs(epsilon)) ** 2) * vTe / omgpe
+        # SKW_ele_omgE = 2 * jnp.pi * 1.0 / klde * (ele_compE) / ((jnp.abs(1 + (chiE))) ** 2) * vTe / omgpe
+
+        PsOmg = (SKW_ion_omg + SKW_ele_omg) * (1 + 2 * omgdop / omgL) * re**2.0 * jnp.transpose(ne)
+        # PsOmgE = (SKW_ele_omgE) * (1 + 2 * omgdop / omgL) * re**2.0 * jnp.transpose(ne)
+        lams = 2 * jnp.pi * C / omgs
+        lams = lams[jnp.newaxis, ..., jnp.newaxis]
+        PsLam = PsOmg * 2 * jnp.pi * C / lams**2
+        # PsLamE = PsOmgE * 2 * jnp.pi * C / lams**2
+
+        formfactor = PsLam
+        # formfactorE = PsLamE
+
+        return formfactor, lams
+
+    return nonMaxwThomson
--- run.py
+++ run.py
@@ -3,7 +3,8 @@
 
 import time
 import matplotlib.pyplot as plt
-from inverse_thomson_scattering.v0 import form_factor
+from inverse_thomson_scattering.v0 import form_factor as np_ff
+from inverse_thomson_scattering.jax import form_factor as jnp_ff
 import numpy as np
 
 
@@ -11,12 +12,18 @@ if __name__ == "__main__":
     x = np.array(np.arange(-8, 8, 0.1))
     distf = 1 / (2 * np.pi) ** (1 / 2) * np.exp(-(x**2) / 2)
     sa = np.linspace(55, 65, 10)
+    backend = "jax"
+
+    if backend == "numpy":
+        [formf, lams] = np_ff.nonMaxwThomson(1, 1, 1, 1, 1, 0.3e20, 0, 0, [400, 700], 526.5, sa, distf, x)
+    elif backend == "jax":
+        ff_fn = jnp_ff.get_form_factor_fn([400, 700], 526.5)
+        [formf, lams] = ff_fn(1, 1, 1, 1, 1, 0.3e20, 0, 0, sa, (distf, x))
 
     t0 = time.time()
+    [formf, lams] = np_ff.nonMaxwThomson(1, 1, 1, 1, 1, 0.3e20, 0, 0, [400, 700], 526.5, sa, distf, x)
     # [formf,lams]=nonMaxwThomson(1,1,[1,2],[1,4],[.5, .5],.3e20,0,0,[400,700],526.5,sa,distf,x)
-    [formf, lams] = form_factor.nonMaxwThomson(1, 1, 1, 1, 1, 0.3e20, 0, 0, [400, 700], 526.5, sa, distf, x)
     t1 = time.time()
-
     # print(formf[0,:,1])
     print(t1 - t0)
     plt.plot(formf[0, :, 0])
