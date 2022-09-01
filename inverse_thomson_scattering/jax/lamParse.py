375bb45181966e06d6eee44e0a02ef8f134a7e69
archis <joglekara@users.noreply.github.com>
2022-08-31 21:09:07 -0700

    removing a bool

--- inverse_thomson_scattering/jax/lamParse.py
+++ inverse_thomson_scattering/jax/lamParse.py
@@ -35,20 +35,20 @@ def lamParse(lamrang, lam, npts, fineion=True):
     max_lam = lamrang[1]
     npts = 102500
 
-    if fineion and (min_lam < lam and max_lam > lam):
-        lamAxis = jnp.linspace(min_lam, max_lam, npts)
-        L = next(i for i in range(len(lamAxis)) if lamAxis[i] >= lam - 2)
-        # print(L)
-        # L = find(lamAxis >= lam - 2, 1, 'first');
-        rlamAxis = lamAxis[::-1]
-        R = jnp.abs(next(i for i in range(len(lamAxis)) if rlamAxis[i] <= lam + 2) - len(lamAxis))
-        # R = find(lamAxis <= lam + 2, 1, 'last');
-        V = jnp.linspace(lamAxis[L], lamAxis[R], npts)
-        # V = linspace(lamAxis(L), lamAxis(R), npts);
-        lamAxis = jnp.concatenate((lamAxis[0:L], V, lamAxis[R + 1 : -1]))
-
-    else:
-        lamAxis = jnp.linspace(min_lam, max_lam, npts)
+    # if fineion and (min_lam < lam and max_lam > lam):
+    #     lamAxis = jnp.linspace(min_lam, max_lam, npts)
+    #     L = next(i for i in range(len(lamAxis)) if lamAxis[i] >= lam - 2)
+    #     # print(L)
+    #     # L = find(lamAxis >= lam - 2, 1, 'first');
+    #     rlamAxis = lamAxis[::-1]
+    #     R = jnp.abs(next(i for i in range(len(lamAxis)) if rlamAxis[i] <= lam + 2) - len(lamAxis))
+    #     # R = find(lamAxis <= lam + 2, 1, 'last');
+    #     V = jnp.linspace(lamAxis[L], lamAxis[R], npts)
+    #     # V = linspace(lamAxis(L), lamAxis(R), npts);
+    #     lamAxis = jnp.concatenate((lamAxis[0:L], V, lamAxis[R + 1 : -1]))
+    #
+    # else:
+    lamAxis = jnp.linspace(min_lam, max_lam, npts)
 
     omgs = 2e7 * jnp.pi * c / lamAxis  # Scattered frequency axis(1 / sec)
     omgL = 2 * jnp.pi * 1e7 * c / lam  # laser frequency Rad / s
