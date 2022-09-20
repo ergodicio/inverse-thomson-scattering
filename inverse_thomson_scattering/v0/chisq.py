50cde99c63375a50aa606167324325c2e4c18d46
archis <joglekara@users.noreply.github.com>
2022-09-20 16:47:17 -0700

    jit spectra

--- inverse_thomson_scattering/v0/chisq.py
+++ inverse_thomson_scattering/v0/chisq.py
@@ -56,6 +56,7 @@ def get_loss_function(TSinputs, xie, sas, data):
 
         return lamAxisE, ThryE
 
+    @jit
     def get_spectra(modlE, modlI, lamAxisE, lamAxisI, amps):
 
         if TSinputs["D"]["extraoptions"]["load_ion_spec"]:
