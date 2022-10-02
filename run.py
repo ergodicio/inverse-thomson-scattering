009e99c00501f5cf88c0f4cd9d17b4fc0245a419
archis <joglekara@users.noreply.github.com>
2022-10-02 09:08:43 -0700

    flatten dict

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     with mlflow.start_run() as run:
         t0 = time.time()
-        mlflow.log_params(config)
+        mlflow.log_params(flatten(config))
         fit_results = datafitter.fit(config=config)
         metrics_dict = {"datafitter_time": time.time() - t0}  # , "loss": fit_results["loss"]}
         mlflow.log_metrics(metrics=metrics_dict)
