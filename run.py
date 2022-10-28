5eec52cfe20ddd53b32c2e14fbacefebc4665859
archis <joglekara@users.noreply.github.com>
2022-10-27 19:04:28 -0700

    flatdict

--- run.py
+++ run.py
@@ -6,8 +6,24 @@ import numpy as np
 import mlflow
 import os
 from flatten_dict import flatten, unflatten
+import flatdict
 
 from inverse_thomson_scattering.v0 import datafitter
+
+
+def log_params(cfg):
+    flattened_dict = dict(flatdict.FlatDict(cfg, delimiter="."))
+    num_entries = len(flattened_dict.keys())
+
+    if num_entries > 100:
+        num_batches = num_entries % 100
+        fl_list = list(flattened_dict.items())
+        for i in range(num_batches):
+            end_ind = min((i + 1) * 100, num_entries)
+            trunc_dict = {k: v for k, v in fl_list[i * 100 : end_ind]}
+            mlflow.log_params(trunc_dict)
+    else:
+        mlflow.log_params(flattened_dict)
 
 
 def update(base_dict, new_dict):
@@ -43,7 +59,7 @@ if __name__ == "__main__":
         mlflow.set_experiment(config["mlflow"]["experiment"])
 
         with mlflow.start_run() as run:
-            mlflow.log_params(defaults)
+            log_params(config)
             with tempfile.TemporaryDirectory() as td:
                 with open(os.path.join(td, "config.yaml", "w")) as fi:
                     yaml.safe_dump(config, fi)
