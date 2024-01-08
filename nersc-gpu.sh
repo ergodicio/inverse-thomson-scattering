834c9e36b28f4ad6c58abc176136b9c61119a005
Avram Milder <amilder@login38.chn.perlmutter.nersc.gov>
2024-01-08 11:10:26 -0800

    input deck change

--- nersc-gpu.sh
+++ nersc-gpu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -A m4434_g
 #SBATCH -C gpu
-#SBATCH -q shared
-#SBATCH -t 2:00:00
+#SBATCH -q debug
+#SBATCH -t 0:30:00
 #SBATCH -n 1
 #SBATCH -c 32
 #SBATCH --gpus-per-task=1
@@ -10,12 +10,13 @@
 export SLURM_CPU_BIND="cores"
 export BASE_TEMPDIR="$PSCRATCH/tmp/"
 export MLFLOW_TRACKING_URI="$PSCRATCH/mlflow"
+export MLFLOW_EXPORT=True
 
 # copy job stuff over
 module load python
 module load cudnn/8.9.3_cuda12.lua
 module load cudatoolkit/12.0.lua
 
-mamba activate tsadar-gpu
-cd /global/u2/a/<path>/
-# the run command will be appended here
+
+conda activate tsadar-gpu
+cd /global/homes/a/amilder/inverse-thomson-scattering
